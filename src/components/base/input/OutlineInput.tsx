e303a646413d007ffbdb05d4cd76449b37af78d3
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-10-25 12:31:45 +0900

    Fix: 1000 divide error

--- src/components/base/input/OutlineInput.tsx
+++ src/components/base/input/OutlineInput.tsx
@@ -1,40 +1,48 @@
 import { InputProp } from '@/types/Input';
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
 
 export type OutlineInputT = {
   color: string;
   fontSize: number;
   bordercolor: string;
-}
+};
 
-  const OutlineInputElement = styled.input<OutlineInputT>`
+const OutlineInputElement = styled.input<OutlineInputT>`
   width: 100%;
   min-width: 70px;
   height: 100%;
   min-height: 30px;
-  font-size: ${(props) => `${(props.fontSize)}px`};
+  font-size: ${(props) => `${props.fontSize}px`};
   color: ${(props) => props.color};
   border: 1px solid ${(props) => props.bordercolor};
   border-radius: 10px;
   text-align: left;
   padding-left: 10px;
   outline: ${(props) => props.bordercolor};
-`
+`;
 
-const OutlineInput = ({ placeholder, updateInput, submitInput, currentValue, color, fontSize, bordercolor }: InputProp & OutlineInputT) => {
-  const devide = parseFloat(currentValue) / 1000;
+const OutlineInput = ({
+  placeholder,
+  updateInput,
+  submitInput,
+  currentValue,
+  color,
+  fontSize,
+  bordercolor,
+}: InputProp & OutlineInputT) => {
+  const devide = parseFloat(currentValue);
   return (
-      <OutlineInputElement
-        placeholder={placeholder}
-        onChange={(event) => updateInput(event)}
-        onKeyDown={submitInput}
-        color={color}
-        fontSize={fontSize}
-        bordercolor={bordercolor}
-        value={devide.toString() || ''}
-      />  
-    )
-}
+    <OutlineInputElement
+      placeholder={placeholder}
+      onChange={(event) => updateInput(event)}
+      onKeyDown={submitInput}
+      color={color}
+      fontSize={fontSize}
+      bordercolor={bordercolor}
+      value={devide.toString() || ''}
+    />
+  );
+};
 
-export default OutlineInput
+export default OutlineInput;
