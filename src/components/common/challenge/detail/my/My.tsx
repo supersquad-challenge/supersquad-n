03a6764352fa8a29909230bbc16efb88e842b213
Tina Lee <92625071+Learning-N-Running@users.noreply.github.com>
2023-10-27 00:34:32 +0900

    Fix: number error

--- src/components/common/challenge/detail/my/My.tsx
+++ src/components/common/challenge/detail/my/My.tsx
@@ -6,72 +6,73 @@ import { getUserStatus } from '@/lib/api/querys/user/getUserStatus';
 import { UserStatus } from '@/types/user/User';
 import { convertIsoDateToReadable } from '@/utils/dateFormatUtils';
 import { thousandFormat } from '@/utils/moneyFormatUtils';
-import React from 'react'
+import React from 'react';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 
 type Props = {
   id: string;
-}
+};
 
 const My = ({ id }: Props) => {
   const { data, isLoading, error } = useQuery({
     queryKey: [`my-${id}`],
     queryFn: async () => {
       const res = await getUserStatus({
-        userChallengeId: id
-      })
+        userChallengeId: id,
+      });
       const myStatus: UserStatus = res.myStatus;
       return myStatus;
     },
     staleTime: 5000,
-    cacheTime: Infinity
-  })
+    cacheTime: Infinity,
+  });
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
-  
-  if (!isLoading && (error
-    || data === undefined || data === null)) {
+
+  if (!isLoading && (error || data === undefined || data === null)) {
     return <CommonError msg="Fetch failed" />;
   }
-  
+
   return (
     <Container>
       <LongBlock
-        leftTitle='My Success Rate'
+        leftTitle="My Success Rate"
         leftContent={`${thousandFormat(data.successRate)}%`}
-        rightTitle='Target Success'
+        rightTitle="Target Success"
         rightContent={'100%'}
-        leftColor='#000000'
-        rightColor='#000000'
-        leftBackground='#F6F6F6'
-        rightBackground='#F6F6F6'
+        leftColor="#000000"
+        rightColor="#000000"
+        leftBackground="#F6F6F6"
+        rightBackground="#F6F6F6"
       >
-        <ProgressBar
-          rate={data.successRate}
-        />
+        <ProgressBar rate={data.successRate} />
       </LongBlock>
       <LongBlock
-        leftTitle='Your Deposit'
-        leftContent={`$${thousandFormat(data.deposit)}`}
-        rightTitle='Availability'
-        rightContent={data.challengeEndsAt.length === 0 ? 'None' : convertIsoDateToReadable(data.challengeEndsAt)}
-        leftColor='#000000'
-        rightColor='#000000'
-        leftBackground='#F6F6F6'
-        rightBackground='#F6F6F6'
+        leftTitle="Your Deposit"
+        leftContent={`$20`}
+        rightTitle="Availability"
+        rightContent={
+          data.challengeEndsAt.length === 0
+            ? 'None'
+            : convertIsoDateToReadable(data.challengeEndsAt)
+        }
+        leftColor="#000000"
+        rightColor="#000000"
+        leftBackground="#F6F6F6"
+        rightBackground="#F6F6F6"
       ></LongBlock>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.section`
   width: 100%;
   min-width: 250px;
   max-width: 450px;
   margin: 0 auto;
-`
+`;
 
-export default My
+export default My;
